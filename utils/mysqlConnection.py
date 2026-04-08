0c8131fe57080c6d077e13ac5a55f32948e894d7
kedns <kednsve@163.com>
2026-04-08 15:52:12 +0800

    utils - engine

---
+++ utils/mysqlConnection.py
@@ -0,0 +1,26 @@
+import pymysql
+import sqlalchemy
+
+
+class ConnectMysql:
+    def __init__(self):
+        self.host = 'localhost'
+        self.port = '3306'
+        self.user = 'root'
+        self.password = '123456'
+        self.database = 'data_analysis'
+        self.charset = 'utf8'
+
+    def get_engine(self):
+        url = (
+                'mysql+pymysql://' + self.user + ':' + self.password +
+                '@' + self.host + ':' + self.port + '/' + self.database +
+                '?charset=' + self.charset
+        )
+        engine = sqlalchemy.create_engine(url)
+        return engine
+
+
+if __name__ == '__main__':
+    engine = ConnectMysql().get_engine()
+    engine.connect()
