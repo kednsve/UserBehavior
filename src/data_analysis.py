060c88ef1ebf1c43e2e851a0510aed1f6fd1c083
kedns <kednsve@163.com>
2026-04-10 16:47:24 +0800

    user activity -- plot

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,7 +1,10 @@
 from utils.mysqlConnection import ConnectMysql
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+plt.rcParams['font.sans-serif'] = ['SimHei']
 """
 1.用户流量分析：PV 用户活跃时段
 2.转化漏斗分析：浏览->购买
@@ -27,10 +30,12 @@ def csv_to_sql():
     data['datetime'] = pd.to_datetime(data['timestamp'], unit='s')
     # 添加列：date     yyyy-MM-dd
     data['date'] = data['datetime'].dt.date
+    data['date'] = pd.to_datetime(data['date'])
+    data=data[(data['date']>= '2017-11-25') & (data['date'] <= '2017-12-03')]
     # 添加列：month
-    data['month'] = data['date'].dt.month
+    data['month'] = data['datetime'].dt.month
     # 添加列：hour
-    data['hour'] = data['date'].dt.hour
+    data['hour'] = data['datetime'].dt.hour
     data.info()
     # 无缺失值
     print(data.head(5))
@@ -56,14 +61,36 @@ def user_activity():
     # 筛选出浏览与购买数据
     pv_data = data[data['behavior'] == 'pv']
     buy_data = data[data['behavior'] == 'buy']
-    # 绘图数据准备
-    x_hours = [i for i in range(24)]
-    x_months = [i for i in range(1, 13)]
-    # TODO
+    del data
+    date_pv = pv_data.groupby('date')['behavior'].count().reset_index(name='pv')
+    date_buy = buy_data.groupby('date')['behavior'].count().reset_index(name='buy')
+    hour_pv = pv_data.groupby('hour')['behavior'].count().reset_index(name='pv')
+    del pv_data,buy_data
+    # 购买转化率
+    buy_rate = pd.DataFrame()
+    buy_rate['date']=date_pv['date']
+    buy_rate['rate']=date_buy['buy']/date_pv['pv']
+    # 画图
+    plt.subplot(2,2,1)
+    sns.lineplot(data=date_pv,x='date',y='pv',label='PV')
+    plt.xticks(rotation=45)
+    plt.title('日PV')
+    plt.subplot(2,2,2)
+    sns.lineplot(data=date_buy,x='date',y='buy',label='Buy')
+    plt.xticks(rotation=45)
+    plt.title('日Buy')
+    plt.subplot(2,2,3)
+    sns.lineplot(data=hour_pv,x='hour',y='pv',label='PV')
+    plt.xticks(rotation=45)
+    plt.title('时PV')
+    plt.subplot(2,2,4)
+    sns.barplot(data=buy_rate,x='date',y='rate',label='Buy Rate')
+    plt.xticks(rotation=45)
+    plt.title('购买转化率')
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
     # csv_to_sql()
     user_activity()
-
-    pass
