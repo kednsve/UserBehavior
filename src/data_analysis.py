5788123134ffaaedf22dc1ec72c755d3236abe8e
kedns <kednsve@163.com>
2026-04-14 20:44:33 +0800

    try black

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,12 +1,12 @@
-from utils.mysqlConnection import ConnectMysql
-
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 from pyecharts import options as opts
-from pyecharts.charts import Funnel, Pie, Bar
+from pyecharts.charts import Funnel, Pie
 
-plt.rcParams['font.sans-serif'] = ['SimHei']
+from utils.mysqlConnection import ConnectMysql
+
+plt.rcParams["font.sans-serif"] = ["SimHei"]
 """
 1.用户流量分析：PV 用户活跃时段
 2.转化漏斗分析：浏览->购买
@@ -21,34 +21,35 @@ plt.rcParams['font.sans-serif'] = ['SimHei']
 # behavior: pv      buy     cart        fav
 #           点击     购买     加入购物车    收藏
 
+
 # 从100M数据中抽取1M，处理后存入数据库中
 def csv_to_sql():
     """
     randomly select 1m data from csv file to MySql table
     """
-    data = pd.read_csv('../data/UserBehavior.csv')
+    data = pd.read_csv("../data/UserBehavior.csv")
     data = data.sample(n=1000000, random_state=22)
     # 添加列：datetime yyyy-MM-dd HH:mm:ss
-    data['datetime'] = pd.to_datetime(data['timestamp'], unit='s')
+    data["datetime"] = pd.to_datetime(data["timestamp"], unit="s")
     # 添加列：date     yyyy-MM-dd
-    data['date'] = data['datetime'].dt.date
-    data['date'] = pd.to_datetime(data['date'])
-    data = data[(data['date'] >= '2017-11-25') & (data['date'] <= '2017-12-03')]
+    data["date"] = data["datetime"].dt.date
+    data["date"] = pd.to_datetime(data["date"])
+    data = data[(data["date"] >= "2017-11-25") & (data["date"] <= "2017-12-03")]
     # 添加列：month
-    data['month'] = data['datetime'].dt.month
+    data["month"] = data["datetime"].dt.month
     # 添加列：hour
-    data['hour'] = data['datetime'].dt.hour
+    data["hour"] = data["datetime"].dt.hour
     data.info()
     # 无缺失值
     print(data.head(5))
     engine = ConnectMysql()
     data.to_sql(
-        'userbehavior',
+        "userbehavior",
         engine.get_engine(),
         index=False,
-        if_exists='replace',
-        method='multi',
-        chunksize=10000
+        if_exists="replace",
+        method="multi",
+        chunksize=10000,
     )
 
 
@@ -56,118 +57,117 @@ def csv_to_sql():
 # user_id product_id category_id behavior timestamp datetime date month hour
 # 用户ID   商品ID     商品类目ID     用户行为  时间戳
 
+
 def basic_analysis():
     engine = ConnectMysql().get_engine()
-    data = pd.read_sql('userbehavior', engine)
+    data = pd.read_sql("userbehavior", engine)
     data.info()
     # 筛选出浏览与购买数据
-    pv_data = data[data['behavior'] == 'pv']
-    buy_data = data[data['behavior'] == 'buy']
+    pv_data = data[data["behavior"] == "pv"]
+    buy_data = data[data["behavior"] == "buy"]
     del data
-    date_pv = pv_data.groupby('date')['behavior'].count().reset_index(name='pv')
-    date_buy = buy_data.groupby('date')['behavior'].count().reset_index(name='buy')
-    hour_pv = pv_data.groupby('hour')['behavior'].count().reset_index(name='pv')
+    date_pv = pv_data.groupby("date")["behavior"].count().reset_index(name="pv")
+    date_buy = buy_data.groupby("date")["behavior"].count().reset_index(name="buy")
+    hour_pv = pv_data.groupby("hour")["behavior"].count().reset_index(name="pv")
     del pv_data, buy_data
     # 购买转化率
     buy_rate = pd.DataFrame()
-    buy_rate['date'] = date_pv['date']
-    buy_rate['rate'] = date_buy['buy'] / date_pv['pv']
+    buy_rate["date"] = date_pv["date"]
+    buy_rate["rate"] = date_buy["buy"] / date_pv["pv"]
     # 画图
     plt.figure(figsize=(12, 8), dpi=100)
     plt.subplot(2, 2, 1)
-    sns.lineplot(data=date_pv, x='date', y='pv', label='PV')
+    sns.lineplot(data=date_pv, x="date", y="pv", label="PV")
     plt.xticks(rotation=15)
-    plt.title('日PV')
+    plt.title("日PV")
     plt.subplot(2, 2, 2)
-    sns.lineplot(data=date_buy, x='date', y='buy', label='Buy')
+    sns.lineplot(data=date_buy, x="date", y="buy", label="Buy")
     plt.xticks(rotation=15)
-    plt.title('日Buy')
+    plt.title("日Buy")
     plt.subplot(2, 2, 3)
-    sns.lineplot(data=hour_pv, x='hour', y='pv', label='PV')
-    plt.title('时PV')
+    sns.lineplot(data=hour_pv, x="hour", y="pv", label="PV")
+    plt.title("时PV")
     plt.subplot(2, 2, 4)
-    sns.barplot(data=buy_rate, x='date', y='rate', label='Buy Rate')
+    sns.barplot(data=buy_rate, x="date", y="rate", label="Buy Rate")
     plt.xticks(rotation=15)
-    plt.title('购买转化率')
+    plt.title("购买转化率")
     plt.tight_layout()
-    plt.savefig('../data/流量分析.jpg')
+    plt.savefig("../data/流量分析.jpg")
     plt.show()
 
 
 def funnel_behavior():
     engine = ConnectMysql().get_engine()
-    data = pd.read_sql('userbehavior', engine)
-    counts = data['behavior'].value_counts()
+    data = pd.read_sql("userbehavior", engine)
+    counts = data["behavior"].value_counts()
     del data
-    pv_cnt = int(counts.get('pv', 0))
-    buy_cnt = int(counts.get('buy', 0))
-    cart_cnt = int(counts.get('cart', 0))
-    fav_cnt = int(counts.get('fav', 0))
-    labels = ['PV', '收藏', '加购', '购买']
+    pv_cnt = int(counts.get("pv", 0))
+    buy_cnt = int(counts.get("buy", 0))
+    cart_cnt = int(counts.get("cart", 0))
+    fav_cnt = int(counts.get("fav", 0))
+    labels = ["PV", "收藏", "加购", "购买"]
     cnts = [pv_cnt, fav_cnt, cart_cnt, buy_cnt]
     funnel = (
         Funnel()
-        .add("转化漏斗",
-             [list(z) for z in zip(labels, cnts)])
+        .add("转化漏斗", [list(z) for z in zip(labels, cnts)])
         .set_global_opts(title_opts=opts.TitleOpts(title="转化漏斗分析"))
     )
-    funnel.render('../data/用户转化漏斗图.html')
+    funnel.render("../data/用户转化漏斗图.html")
     pie = (
         Pie()
-        .add("用户行为分布",
-             [list(z) for z in zip(labels, cnts)])
-        .set_global_opts(title_opts=opts.TitleOpts(title='用户行为分布'))
+        .add("用户行为分布", [list(z) for z in zip(labels, cnts)])
+        .set_global_opts(title_opts=opts.TitleOpts(title="用户行为分布"))
     )
-    pie.render('../data/用户行为分布饼状图.html')
+    pie.render("../data/用户行为分布饼状图.html")
 
 
 def hot_analysis():
     engine = ConnectMysql().get_engine()
-    data = pd.read_sql('userbehavior', engine)
+    data = pd.read_sql("userbehavior", engine)
     hot_pv = (
-        data[data['behavior'] == 'pv']
-        .groupby('product_id')
+        data[data["behavior"] == "pv"]
+        .groupby("product_id")
         .size()
-        .reset_index(name='pv')
-        .sort_values('pv', ascending=False)
+        .reset_index(name="pv")
+        .sort_values("pv", ascending=False)
         .head(10)
     )
     hot_buy = (
-        data[data['behavior'] == 'buy']
-        .groupby('product_id')
+        data[data["behavior"] == "buy"]
+        .groupby("product_id")
         .size()
-        .reset_index(name='buy')
-        .sort_values('buy', ascending=False)
+        .reset_index(name="buy")
+        .sort_values("buy", ascending=False)
         .head(10)
     )
     hot_cate = (
-        data[data['behavior'] == 'buy']
-        .groupby('category_id')
+        data[data["behavior"] == "buy"]
+        .groupby("category_id")
         .size()
-        .reset_index(name='buy')
-        .sort_values('buy', ascending=False)
+        .reset_index(name="buy")
+        .sort_values("buy", ascending=False)
         .head(10)
     )
-    plt.figure(figsize=(5,12), dpi=100)
+    plt.figure(figsize=(5, 12), dpi=100)
     plt.subplot(3, 1, 1)
-    plt.bar(hot_pv['product_id'].astype(str),hot_pv['pv'])
+    plt.bar(hot_pv["product_id"].astype(str), hot_pv["pv"])
     plt.xticks(rotation=25)
-    plt.title('hot_pv')
+    plt.title("hot_pv")
     plt.subplot(3, 1, 2)
-    plt.bar(hot_cate['category_id'].astype(str),hot_cate['buy'])
+    plt.bar(hot_cate["category_id"].astype(str), hot_cate["buy"])
     plt.xticks(rotation=25)
-    plt.title('hot_cate')
+    plt.title("hot_cate")
     plt.subplot(3, 1, 3)
-    plt.bar(hot_buy['product_id'].astype(str),hot_buy['buy'])
+    plt.bar(hot_buy["product_id"].astype(str), hot_buy["buy"])
     plt.xticks(rotation=25)
-    plt.title('hot_buy')
-    plt.suptitle('热门商品Top10')
+    plt.title("hot_buy")
+    plt.suptitle("热门商品Top10")
     plt.tight_layout()
-    plt.savefig('../data/热门商品TOP10.jpg')
+    plt.savefig("../data/热门商品TOP10.jpg")
     plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # csv_to_sql()
     # basic_analysis()
     # funnel_behavior()
