ac8c8845e1fe2c1dea9cde8f93e75ab73e15871e
kedns <kednsve@163.com>
2026-04-14 20:37:35 +0800

    hot_analysis -- 热门商品TOP10柱状图

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from pyecharts import options as opts
-from pyecharts.charts import Funnel, Pie
+from pyecharts.charts import Funnel, Pie, Bar
 
 plt.rcParams['font.sans-serif'] = ['SimHei']
 """
@@ -140,14 +140,31 @@ def hot_analysis():
         .sort_values('buy', ascending=False)
         .head(10)
     )
-    hot_buy = (
+    hot_cate = (
         data[data['behavior'] == 'buy']
-        .groupby('product_id')
+        .groupby('category_id')
         .size()
         .reset_index(name='buy')
         .sort_values('buy', ascending=False)
         .head(10)
     )
+    plt.figure(figsize=(5,12), dpi=100)
+    plt.subplot(3, 1, 1)
+    plt.bar(hot_pv['product_id'].astype(str),hot_pv['pv'])
+    plt.xticks(rotation=25)
+    plt.title('hot_pv')
+    plt.subplot(3, 1, 2)
+    plt.bar(hot_cate['category_id'].astype(str),hot_cate['buy'])
+    plt.xticks(rotation=25)
+    plt.title('hot_cate')
+    plt.subplot(3, 1, 3)
+    plt.bar(hot_buy['product_id'].astype(str),hot_buy['buy'])
+    plt.xticks(rotation=25)
+    plt.title('hot_buy')
+    plt.suptitle('热门商品Top10')
+    plt.tight_layout()
+    plt.savefig('../data/热门商品TOP10.jpg')
+    plt.show()
 
 
 if __name__ == '__main__':
